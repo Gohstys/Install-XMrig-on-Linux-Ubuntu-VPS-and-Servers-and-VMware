aacf9dc5bfb4a94737b3a13d2b87d8a859ca868e
Gohsty <89268998+Gohstys@users.noreply.github.com>
2022-12-08 15:29:35 +0100

    Create advancedconstruction.sh

---
+++ advancedconstruction.sh
@@ -0,0 +1,1 @@
+sudo apt-get install git build-essential cmake automake libtool autoconf && git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/scripts && ./build_deps.sh && cd ../build && cmake .. -DXMRIG_DEPS=scripts/deps && make -j$(nproc)
