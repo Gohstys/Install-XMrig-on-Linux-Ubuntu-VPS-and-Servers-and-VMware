2441ec32348e90850d9a2ec15ae1062fd061f54b
Gohsty <89268998+Gohstys@users.noreply.github.com>
2022-12-08 15:31:33 +0100

    Create MINERxCUDA.sh
    Compiling the CUDA plugin is optional and only required if you want to use NVIDIA GPUs.
    
    Follow the instructions at https://developer.nvidia.com/cuda-downloads to install CUDA.

---
+++ MINERxCUDA.sh
@@ -0,0 +1,1 @@
+sudo apt-get install git build-essential cmake automake libtool autoconf && git 1. git clone https://github.com/xmrig/xmrig-cuda.git && mkdir xmrig-cuda/build && cd xmrig-cuda/build && cmake .. -DCUDA_LIB=/usr/local/cuda/lib64/stubs/libcuda.so -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda && make -j$(nproc)
