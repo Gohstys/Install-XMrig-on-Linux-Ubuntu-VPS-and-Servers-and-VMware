67a0dcc6ee717e53eac21812a4b8198687301499
Gohsty <89268998+Gohstys@users.noreply.github.com>
2022-12-08 15:27:15 +0100

    Create basicconstruction.sh
    To start use: xmrig/build/./xmrig -o stratum+tcp://prohashing.com:3359 -u sabiasque -p a=randomx,n=I_TA_VPS,o=VPS -k --coin monero -a rx/0
    
    or
    
    sudo ./xmrig -o POOL:3333 -u WALLED -k

---
+++ basicconstruction.sh
@@ -0,0 +1,1 @@
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/build && cmake .. && make -j$(nproc)
